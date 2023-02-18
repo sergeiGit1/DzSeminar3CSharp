3eca65b57cd6ffd738a073b1475563db2c464b51
sergei <dashkosergeiit@gmail.com>
2023-02-19 00:04:58 +0300

    готов доп 1, задача 21 и задача 23

---
+++ additionalTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Доп. задание №1:
+// Представим банк, в котором алгоритм начисления процента по вкладу 
+// зависит от суммы вклада. На вход будет подаваться число (сумма вклада). 
+// При значении меньше 100, будет начислено 5 %, если значение находится в диапазоне 
+// от ста до двухсот — 7 %, если больше — 10 %. Отработав, программа должна вывести 
+// общую сумму с начисленными процентами.
+Console.WriteLine("Введите сумму вклада: ");
+double depAmount = Convert.ToInt32(Console.ReadLine());
+double addSum = 0;
+if (depAmount <= 100)
+{
+    addSum = (depAmount / 100) * 5;
+    Console.WriteLine($"Общая сумма: {depAmount + addSum}");
+}
+else if (depAmount >= 100 && depAmount <= 200)
+{
+    addSum = (depAmount / 100) * 7;
+    Console.WriteLine($"Общая сумма: {depAmount + addSum}");
+}
+else if (depAmount >= 200)
+{
+    addSum = (depAmount / 100) * 10;
+    Console.WriteLine($"Общая сумма: {depAmount + addSum}");
+}
+
---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и 
+//находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Введите координату точки А по оси X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по оси Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по оси Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по оси X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки B по оси Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки B по оси Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow(x1 - x, 2) + Math.Pow(y1 - y, 2) + Math.Pow(z1 - z, 2));
+
+Console.WriteLine(Math.Round(result, 2));
